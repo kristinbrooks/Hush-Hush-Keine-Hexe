d0553e4ddc1d0032e7a92a66f05a1039dc93dfe8
Kristin Brooks <kristin.ruth.brooks@gmail.com>
2019-08-04 01:54:48 -0700

    working on the shuffle

--- src/java/CIS129_KristinBrooks_Turn.java
+++ src/java/CIS129_KristinBrooks_Turn.java
@@ -14,7 +14,21 @@ public class CIS129_KristinBrooks_Turn {
 
     // METHODS
     public void take() {
-        System.out.print(rollDie());
+        String roll = rollDie();
+        roll = "S";
+
+        if (roll.equals("S")) {
+            System.out.print("You rolled " + roll + ". Enter column numbers 'c1,c2' to shuffle or 'x' to end your turn: ");
+            String input = "1,3";
+            String[] columns = input.split(",");
+            shuffle(Integer.parseInt(columns[0]), Integer.parseInt(columns[1]));
+        }
+
+    }
+
+    private void shuffle(int column1, int column2) {
+        System.out.println(column1);
+        System.out.println(column2);
     }
 
     private String rollDie() {
